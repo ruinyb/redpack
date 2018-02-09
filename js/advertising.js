b7a0b705207a3f09e655a2e02c031f6845ec5145
pengliheng <pengliheng@qq.com>
2018-02-09 21:01:34 +0800

    Revert "红包分会场"
    This reverts commit 9de85990a57321b7a4f82e80dc9b0e506b5c6dc0.

--- js/advertising.js
+++ js/advertising.js
@@ -8,26 +8,4 @@ $(document).ready(function(){
 	        centeredSlides: true,
 	        autoplayDisableOnInteraction: false
 	    });
-
-	/**** 获取地址栏参数 ****/
-	function getQueryString(name) {
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-		var r = window.location.search.substr(1).match(reg);
-		if (r != null) return unescape(r[2]); return null;
-	}
-
-	var id=getQueryString('id');
-	/**** 获取地址栏参数 ****/
-
-		 if(id=='wxf21b6048d08dd3e4'){
-		 	//青年网商
-			$('.swiper-slide .list li img').attr('src','../image/qnws/3.jpg')
-		 }else if(id==''){
-			 //星火
-			 $('.swiper-slide .list li img').attr('src','../image/xinghuo/3.jpg')
-		 }else if(id==''){
-			 //邮储
-			 $('.swiper-slide .list li img').attr('src','../image/youchu/4.jpg')
-		 }
-
 });
